242e5718ea9d9db7660caacda55b9be9fe6468a5
Rafal Stasiorowski <rstasiorowski@gmail.com>
2020-02-29 12:01:51 +0100

    Utworzono pliki .json dla obiektów  student

--- src/pl/sda/student/Main.java
+++ src/pl/sda/student/Main.java
@@ -1,9 +1,10 @@
 package pl.sda.student;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
 
@@ -13,12 +14,27 @@ public class Main {
 
         ObjectMapper objectMapper = new ObjectMapper();
 
+
+
+        Student student1 = new Student("John", "Doe", 33);
+        Student student2 = new Student("Jane", "Doe", 34);
+        Student student3 = new Student("Jan", "Nowak", 35);
+        Student student4 = new Student("Karyna", "Jabol", 36);
+        Student student5 = new Student("Sebastian", "Wisniewski", 37);
         try {
             objectMapper.writeValue(new File("student.json"), student);
+            objectMapper.writeValue(new File("student1.json"), student1);
+            objectMapper.writeValue(new File("student2.json"), student2);
+            objectMapper.writeValue(new File("student3.json"), student3);
+            objectMapper.writeValue(new File("student4.json"), student4);
+            objectMapper.writeValue(new File("student5.json"), student5);
         } catch (IOException e) {
             e.printStackTrace();
         }
+        
 
+//        List<Student>studentList = new ArrayList<>();
+//        studentList.add(student);
 
     }
 }
